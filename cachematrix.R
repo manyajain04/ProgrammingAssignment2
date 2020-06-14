917fe8354a7564947fbc76dc681356af4990d780
manyajain04 <51513312+manyajain04@users.noreply.github.com>
2020-06-15 01:10:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,14 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
+  inv <- NULL 
+  set <- function(y){ ##set the new matrix to your variablex which is initialised as a matrix vector.
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  get <- function() x ##get the matrix that you have set previously.
+  setinverse <- function(inverse) inv <<- inverse ##set your matrix passed as an argument to the function to the inv variable. 
+  getinverse <- function() inv ##get the inverse of mmatrix.
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -20,14 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if (!is.null(inv)){
+  inv <- x$getinverse() ##get the inverse of matrix that you have set in the makeCacheMatrix function call.
+  if (!is.null(inv)){ ##check if it is null or not.
     message('getting cached data')
     return(inv)
   }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinverse(inv)
+  data <- x$get() ##get the value of the matrix passed oriiginally to the first makeCacheMatrix function call.
+  inv <- solve(data, ...) ##use solve function to find inverse of matrix.
+  x$setinverse(inv) ##reset the inv variable according to the inverse of matrix. If it is same as inv in first line, there is no change else, the value is changed to the inverse of the matrix passed originally.
   inv
         ## Return a matrix that is the inverse of 'x'
 }
